8b56e86c46d53ad3298f4f4f0b727122fd15cabd
Michael Lenz <mlenz@calpoly.edu>
2012-11-15 13:53:19 -0800

    falling platforms OMGGIDMOGMSOGNSOIDGFMDSPGMOIS!!!
    K

---
+++ FallingPlatform.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class FallingPlatform here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FallingPlatform extends Platform
+{
+    private int yVel=0;
+    private boolean isFalling=false;
+    /**
+     * Act - do whatever the FallingPlatform wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(isFalling){
+            for(int i=-((getWidth()-4)/2);i<getWidth()-4;i++){
+                Actor block=getOneObjectAtOffset(i,getHeight()/2,Block.class);
+                if(block!=null){
+                   isFalling=false;
+                   return;
+                }
+                if(yVel!=10){
+                    yVel++;
+                }
+                setWorldLocation(getX(),getY()+yVel);
+            }
+            
+        }else{
+               for(int i=-(getWidth()/2);i<getWidth();i++){
+                Actor player=getOneObjectAtOffset(i,-getHeight()/2,Player.class);
+                if(player!=null){
+                   isFalling=true;
+                   
+                }
+            }
+        }
+        // Add your action code here.
+    }    
+}
