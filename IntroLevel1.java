d4a2ce566a000bf3531dba0f518879cacf664f17
Jordan.Tang <jordantang0229@gmail.com>
2012-11-26 13:00:09 -0800

    Couple of levels
    Intro levels involving basic mechanics, focusing on only platforming.

---
+++ IntroLevel1.java
@@ -0,0 +1,88 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class IntroLevel1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class IntroLevel1 extends ShiftWorld
+{
+
+    /**
+     * Constructor for objects of class IntroLevel1.
+     * 
+     */
+    public IntroLevel1()
+    {    
+        super();
+        
+        Player p = new Player();
+        registerPlayer(p, 100, 200);
+        
+        Background bg = new Background("background v2.png");
+        addObject(bg);
+        
+        prepare();
+    }
+    public void prepare()
+    {
+         Block block = new Block();
+        addObject(block, 29, 585);
+        block.blockRow(15);
+        Wall wall = new Wall();
+        addObject(wall, 805, 535);
+        wall.wallStack(5);
+        Platform platform = new Platform();
+        addObject(platform, 717, 251);
+        Platform platform2 = new Platform();
+        addObject(platform2, 513, 359);
+        Platform platform3 = new Platform();
+        addObject(platform3, 268, 469);
+        JumpPad jumppad = new JumpPad(30, 30);
+        addObject(jumppad, 877, 537);
+        Platform platform4 = new Platform();
+        addObject(platform4, 1118, 248);
+        Platform platform5 = new Platform();
+        addObject(platform5, 1402, 186);
+        Platform platform6 = new Platform();
+        addObject(platform6, 1654, 247);
+        Wall wall2 = new Wall();
+        addObject(wall2, 1826, 568);
+        JumpPad jumppad2 = new JumpPad(30, 30);
+        addObject(jumppad2, 1826, 505);
+        Platform platform7 = new Platform();
+        addObject(platform7, 2089, 249);
+        Wall wall3 = new Wall();
+        addObject(wall3, 2181, 567);
+        wall3.wallStack(6);
+        Block block2 = new Block();
+        addObject(block2, 2239, 217);
+        block2.blockRow(3);
+        Platform platform8 = new Platform();
+        addObject(platform8, 2464, 220);
+        Wall wall5 = new Wall();
+        addObject(wall5, 2553, 203);
+        wall5.wallStack(4);
+        removeObject(platform8);
+        FallingPlatform fallingplatform = new FallingPlatform();
+        addObject(fallingplatform, 2464, 221);
+        Wall wall6 = new Wall();
+        addObject(wall6, 2560, 567);
+        JumpPad jumppad3 = new JumpPad(30, 30);
+        addObject(jumppad3, 2561, 504);
+        Platform platform9 = new Platform();
+        addObject(platform9, 2641, 222);
+        Block block3 = new Block();
+        addObject(block3, 2738, 221);
+        block3.blockRow(3);
+        Wall wall7 = new Wall();
+        addObject(wall7, 2731, 567);
+        wall7.wallStack(6);
+        Block block4 = new Block();
+        addObject(block4, 2789, 586);
+        block4.blockRow(5);
+        Exit exit = new Exit();
+        addObject(exit, 2806, 484);
+    }
+}
---
+++ IntroLevel2.java
@@ -0,0 +1,136 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class IntroLevel2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class IntroLevel2 extends ShiftWorld
+{
+
+    /**
+     * Constructor for objects of class IntroLevel2.
+     * 
+     */
+    public IntroLevel2()
+    {    
+        super();
+        
+        Player p = new Player();
+        registerPlayer(p, 50, 200);
+        
+        Background bg = new Background("background.png");
+        addObject(bg);
+        
+        prepare();
+    }
+    public void prepare()
+    {
+         Block block = new Block();
+        addObject(block, 30, 584);
+        block.blockRow(3);
+        JumpPad jumppad = new JumpPad(30, 30);
+        addObject(jumppad, 160, 536);
+        Platform platform = new Platform();
+        addObject(platform, 310, 179);
+        PlatformWaypoint platformwaypoint = new PlatformWaypoint();
+        addObject(platformwaypoint, 387, 181);
+        PlatformWaypoint platformwaypoint2 = new PlatformWaypoint();
+        addObject(platformwaypoint2, 691, 174);
+        MovingPlatform movingplatform = new MovingPlatform(true);
+        addObject(movingplatform, 462, 180);
+        PlatformWaypoint platformwaypoint3 = new PlatformWaypoint();
+        addObject(platformwaypoint3, 688, 432);
+        MovingPlatform movingplatform2 = new MovingPlatform(false);
+        addObject(movingplatform2, 693, 406);
+        Platform platform3 = new Platform();
+        addObject(platform3, 1028, 402);
+        PlatformWaypoint platformwaypoint4 = new PlatformWaypoint();
+        addObject(platformwaypoint4, 1233, 455);
+        MovingPlatform movingplatform3 = new MovingPlatform(false);
+        addObject(movingplatform3, 1237, 423);
+        PlatformWaypoint platformwaypoint5 = new PlatformWaypoint();
+        addObject(platformwaypoint5, 1224, 87);
+        PlatformWaypoint platformwaypoint6 = new PlatformWaypoint();
+        addObject(platformwaypoint6, 1439, 81);
+        PlatformWaypoint platformwaypoint7 = new PlatformWaypoint();
+        addObject(platformwaypoint7, 1433, 451);
+        MovingPlatform movingplatform4 = new MovingPlatform(false);
+        addObject(movingplatform4, 1449, 111);
+        Block block2 = new Block();
+        addObject(block2, 1546, 110);
+        block2.blockRow(3);
+        Wall wall = new Wall();
+        addObject(wall, 1735, 551);
+        wall.wallStack(7);
+        Wall wall2 = new Wall();
+        addObject(wall2, 1945, 125);
+        wall2.wallStack(4);
+        Wall wall12 = new Wall();
+        addObject(wall12, 2045, 125);
+        wall12.wallStack(4);
+        Wall wall13 = new Wall();
+        addObject(wall13, 1995, 125);
+        wall13.wallStack(4);
+        PlatformWaypoint platformwaypoint8 = new PlatformWaypoint();
+        addObject(platformwaypoint8, 1780, 565);
+        PlatformWaypoint platformwaypoint9 = new PlatformWaypoint();
+        addObject(platformwaypoint9, 2217, 568);
+        MovingPlatform movingplatform5 = new MovingPlatform(true);
+        addObject(movingplatform5, 1856, 568);
+        PlatformWaypoint platformwaypoint10 = new PlatformWaypoint();
+        addObject(platformwaypoint10, 2203, 190);
+        MovingPlatform movingplatform6 = new MovingPlatform(false);
+        addObject(movingplatform6, 2213, 220);
+        Wall wall5 = new Wall();
+        addObject(wall5, 2332, 552);
+        wall5.wallStack(5);
+        Wall wall14 = new Wall();
+        addObject(wall14, 2382, 552);
+        wall14.wallStack(5);
+        Wall wall7 = new Wall();
+        addObject(wall7, 2569, 210);
+        wall7.wallStack(4);
+        Wall wall15 = new Wall();
+        addObject(wall15, 2619, 210);
+        wall15.wallStack(4);
+        Wall wall16 = new Wall();
+        addObject (wall16, 2669, 210);
+        wall16.wallStack(4);
+        Wall wall17 = new Wall();
+        addObject (wall17, 2719, 210);
+        wall17.wallStack(4);
+        Wall wall18 = new Wall();
+        addObject (wall18, 2769, 210);
+        wall18.wallStack(4);
+        PlatformWaypoint platformwaypoint11 = new PlatformWaypoint();
+        addObject(platformwaypoint11, 2413, 571);
+        PlatformWaypoint platformwaypoint12 = new PlatformWaypoint();
+        addObject(platformwaypoint12, 2692, 571);
+        PlatformWaypoint platformwaypoint13 = new PlatformWaypoint();
+        addObject(platformwaypoint13, 2949, 573);
+        PlatformWaypoint platformwaypoint14 = new PlatformWaypoint();
+        addObject(platformwaypoint14, 2942, 171);
+        MovingPlatform movingplatform7 = new MovingPlatform(true);
+        addObject(movingplatform7, 2486, 570);
+        MovingPlatform movingplatform8 = new MovingPlatform(true);
+        addObject(movingplatform8, 2878, 571);
+        MovingPlatform movingplatform9 = new MovingPlatform(false);
+        addObject(movingplatform9, 2951, 537);
+        Wall wall11 = new Wall();
+        addObject(wall11, 3065, 552);
+        wall11.wallStack(6);
+        Wall wall19 = new Wall();
+        addObject(wall19, 3115, 552);
+        wall19.wallStack(6);
+        Wall wall20 = new Wall();
+        addObject(wall20, 3165, 552);
+        wall20.wallStack(6);
+        Wall wall21 = new Wall();
+        addObject(wall21, 3215, 552);
+        wall21.wallStack(6);
+        Exit exit = new Exit();
+        addObject(exit, 3153, 101);
+    }
+}
