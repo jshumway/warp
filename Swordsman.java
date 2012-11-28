4a570ee6133e2dd270b3b69581cf707f2bdd1bb1
Geddy Pence <gwpence@calpoly.edu>
2012-11-28 15:03:22 -0800

    Swordsman animation

--- Swordsman.java
+++ Swordsman.java
@@ -5,7 +5,7 @@ public class Swordsman extends PatrolingEnemy
 {
     /* tweakable */
     private int killDistance = 75;
-
+    private boolean stabbing=false;
     public Swordsman(int dir) {
         super(dir);
         if(dir==1)
@@ -17,17 +17,64 @@ public class Swordsman extends PatrolingEnemy
         image.mirrorHorizontally();
         setImage(image);
     }
-    
+    private int delay=0;
     public void act() {
         super.act();
-        attemptKill();
+        if(!stabbing){
+            attemptKill();
+        }else{
+            if(delay==2){
+                animate();
+                delay=0;
+            }
+            else{
+                delay++;
+            }
+        }
     }
-
-    private void attemptKill() {
+    private int counter=0;
+    public void animate(){
+         setImage(animation[counter]);
+        //will need to make it so it cheacks what direction its looking and changes the image to match
+        counter++;
+        if(counter==length){
+            stabbing=false;
+            kill();
+            setImage("Swordsman.png");
+     }
+     if(getFacing()==1)
+        flipImage();
+     }
+     private void kill(){
         Player player=rayTrace(killDistance, getFacing());
         if (player!=null) {
             player.hit();
         }
     }
+    private void attemptKill() {
+        Player player=rayTrace(killDistance, getFacing());
+        if (player!=null) {
+            //player.hit();
+            setAnimation("Slash", ".png", 8);
+            stabbing=true;
+        }
+    }
+    
+    private int length;
+    private  GreenfootImage[] animation;
+    
+    public void setAnimation(String f,String ft,int animationLength)
+    {
+        animation = new GreenfootImage[animationLength];
+        length=animationLength;
+        for(int i=0;i<length;i++){
+            String file=f+i+ft;
+            animation[i]=new GreenfootImage(file);
+            
+        }
+        counter=0;
+        // Add your action code here.
+    }    
+    
 }
 
